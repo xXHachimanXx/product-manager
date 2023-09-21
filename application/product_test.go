7461ce544afe3dc0aba71872748dcb9461de32a2
xXHachimanXx <globaldep90@gmail.com>
2023-09-21 17:32:36 -0300

    Creating routers

--- application/product_test.go
+++ application/product_test.go
@@ -3,6 +3,7 @@ package application_test
 import (
 	"testing"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 
 	"github.com/xXHachimanXx/product-manager/application"
@@ -20,4 +21,41 @@ func TestPrduct_Enable(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "MockA"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 1
+	err = product.Disable()
+	require.Equal(t, "The price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greather or equal zero", err.Error())
 }
