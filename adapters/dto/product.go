d3197682dbf0ed1d297621a43711e528e84115de
xXHachimanXx <globaldep90@gmail.com>
2023-12-18 22:51:56 -0300

    Creating dto

---
+++ adapters/dto/product.go
@@ -0,0 +1,25 @@
+package dto
+
+import "github.com/xXHachimanXx/product-manager/application"
+
+type ProductDTO struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+	Status string  `json:"status"`
+}
+
+func (p *ProductDTO) Bind(product *application.Product) (*application.Product, error) {
+	if p.ID != "" {
+		product.ID = p.ID
+	}
+	product.Name = p.Name
+	product.Price = p.Price
+	product.Status = p.Status
+
+	if _, err := product.IsValid(); err != nil {
+		return &application.Product{}, err
+	}
+
+	return product, nil
+}
